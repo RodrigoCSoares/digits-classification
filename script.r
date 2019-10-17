3fe045c5eb7c951af3e200cb4d078bfc91a4b052
Rodrigo Soares <soares.c.rodrigo@gmail.com>
2019-10-17 11:48:05 -0300

    Added KNN

--- script.r
+++ script.r
@@ -1,4 +1,5 @@
 library(pixmap)
+library(class)
 
 # Le todos os arquivos do diretorio
 files_list <- list.files(path='./files')
@@ -21,3 +22,16 @@ dataframe <- as.data.frame(mtx)
 for(i in 1:length(files_list)) {
   dataframe$digito[i] <- substr(files_list[i], 1, 1) 
 }
+
+# Classificando os dados
+samples <- sample(1:nrow(dataframe), (0.8 * nrow(dataframe)))
+
+cl <- dataframe[samples, ncol(dataframe)]
+train <- dataframe[samples, -ncol(dataframe)]
+test <- dataframe[-samples, -ncol(dataframe)]
+
+knnResult1 <- knn(train, test, cl)
+knnResult3 <- knn(train, test, cl, k = 3)
+knnResult7 <- knn(train, test, cl, k = 7)
+knnResult9 <- knn(train, test, cl, k = 9)
+expectedResult <- dataframe[-samples, ncol(dataframe)]
